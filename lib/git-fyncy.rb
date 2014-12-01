8f12ff32fd6cb1ab1c718e6be1b6db59fd25f471
Ryan McGowan <mcgowanr@amazon.com>
2014-12-01 09:10:01 -0800

    Change to git repository root or exit.

--- lib/git-fyncy.rb
+++ lib/git-fyncy.rb
@@ -6,7 +6,12 @@ module GitFyncy
   extend Utils
 
   def self.main(*extra_rsync_args)
-    working_dir ||= Dir.pwd
+    working_dir = `git rev-parse --show-toplevel`.chomp
+
+    if working_dir.empty?
+      pexit "Must be in a git repository"
+    end
+
     Dir.chdir working_dir
 
     synchronizer = Synchronizer.new extra_rsync_args
